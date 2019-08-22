2649b395aaf3d827051a34b4934346dc3b49efe4
Marko <majaana@live.com>
2019-08-22 13:21:50 +0300

    4.4: apufunktioita ja yksikkötestejä, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,33 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = {
-    dummy
+const Likes = (blogs) => {
+    let result = 0
+    const likes = blogs.map(item =>  {
+        blogs.length > 0
+            ? result += Number(item.likes)
+            : result
+    })
+    return result
 }
+
+const favoriteBlog = (blogs) => {
+    let likeCount = 0
+    let result = {}
+    blogs.forEach(element => {
+        if(element.likes > likeCount){
+            likeCount = element.likes
+            result = {title: element.title}
+        }
+    })
+    console.log(likeCount)
+    console.log(result)
+    return result
+}
+
+module.exports = {
+    dummy,
+    Likes,
+    favoriteBlog
+}
+
