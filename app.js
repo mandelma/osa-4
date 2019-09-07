3b3dff8c48adb1a0242000a334b8691c154e3284
mandelma <mandlimarko@gmail.com>
2019-09-07 14:23:01 +0300

    4.18: blogilistan laajennus, step 7

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
+
+const loginRouter = require('./controllers/login')
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
@@ -26,6 +29,8 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
+
+app.use('/api/login', loginRouter)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
